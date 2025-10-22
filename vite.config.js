5ced96dc5ea2026820ab28965824dc7f0fe3bda2
zwk <1224826360@qq.com>
2025-10-22 13:15:29 +0900

    20251022-test1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: './',  //资源路径用相对路径
   server: {
     proxy: {
-      '/api': 'http://54.238.146.189:80' // 简写形式
+      '/api': 'http://192.168.50.39:5080' // 简写形式
     }
   }
 })
