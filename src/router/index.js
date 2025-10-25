f8a53fd6710375d3f8111ae0eb115dbd25523e0c
zwk <1224826360@qq.com>
2025-10-25 15:20:17 +0900

    20251025-追加了全局前置守卫，没有登陆时不可直接访问upload界面

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,23 @@ import QuestionManager from "../components/QuestionManager.vue";
 const routes = [
   { path: "/", redirect: "/login" },
   { path: "/login", component: Login },
-  { path: "/upload", component: QuestionManager },
+  { path: "/upload", component: QuestionManager, meta: { requiresAuth: true } },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+// 全局前置守卫
+router.beforeEach((to, _from, next) => {
+  const token = localStorage.getItem("token");
+
+  // 读取 meta.requiresAuth
+  if (to.meta.requiresAuth && !token) {
+    next("/login"); // 没登录就跳回登录页
+  } else {
+    next(); // 否则放行
+  }
 });
 
 export default router;
